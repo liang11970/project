99259dfce633ee28488e8d5fe0a9d1e6a2f9c28d
wangliang <309115319@qq.com>
2016-07-13 11:14:36 +0800

    login

--- app/src/main/java/cn/com/hz_project/view/activity/LoginActivity.java
+++ app/src/main/java/cn/com/hz_project/view/activity/LoginActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v4.app.NotificationCompatSideChannelService;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,31 +13,44 @@ import android.widget.Toast;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.com.projectdemos.R;
 
 public class LoginActivity extends Activity {
-    private Button login;
-    private EditText user;
-    private EditText password;
+    @Bind(R.id.user)
+    EditText user;
+    @Bind(R.id.password)
+    EditText password;
+    @Bind(R.id.login)
+    Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
         if(isNetworkConnected(LoginActivity.this));
             else
             Toast.makeText(this,"当前网络不可用",Toast.LENGTH_SHORT).show();
-        login=(Button)findViewById(R.id.login);
-        user=(EditText)findViewById(R.id.user);
+
         login.setOnClickListener(view -> {
-            if (isMobileNO(String.valueOf(user.getText())));
-                    else Toast.makeText(LoginActivity.this,"当前手机号码有问题，检查一下吧",Toast.LENGTH_SHORT).show();
+            if (isMobileNO(String.valueOf(user.getText()))){
+
+                Toast.makeText(LoginActivity.this,"登录操作",Toast.LENGTH_SHORT).show();
+
+
+            }
+            else Toast.makeText(LoginActivity.this,"当前手机号码有问题，检查一下吧",Toast.LENGTH_SHORT).show();
 
 
         });
 
     }
-    public static boolean isMobileNO(String mobiles){
+
+    public static boolean isMobileNO(String mobiles) {
 
         Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
 
@@ -48,7 +58,8 @@ public class LoginActivity extends Activity {
 
         return m.matches();
 
-        }
+    }
+
     public boolean isNetworkConnected(Context context) {
         if (context != null) {
             ConnectivityManager mConnectivityManager = (ConnectivityManager) context
@@ -60,4 +71,6 @@ public class LoginActivity extends Activity {
         }
         return false;
     }
+
+
 }
