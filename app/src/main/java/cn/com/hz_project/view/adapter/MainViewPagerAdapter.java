aff7ead3990699d43a2fe4e4fcefc29ef7a56b34
kakugg <kakuxwn@163.com>
2016-08-05 09:56:03 +0800

    点击事件

--- app/src/main/java/cn/com/hz_project/view/adapter/MainViewPagerAdapter.java
+++ app/src/main/java/cn/com/hz_project/view/adapter/MainViewPagerAdapter.java
@@ -1,12 +1,14 @@
 package cn.com.hz_project.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -14,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.com.hz_project.model.bean.VideoResult;
+import cn.com.hz_project.view.activity.VideoActivity;
+import cn.com.hz_project.view.activity.VideoListActivity;
 import cn.com.projectdemos.R;
 
 /**
@@ -24,6 +28,8 @@ public class MainViewPagerAdapter extends PagerAdapter {
     private List<VideoResult.ObjBean> tops = new ArrayList<>();
 
     private Context mContext;
+
+    private Intent mIntent;
 
 
     public MainViewPagerAdapter(Context context, List<VideoResult.ObjBean> tops) {
@@ -50,17 +56,27 @@ public class MainViewPagerAdapter extends PagerAdapter {
         View view = LayoutInflater.from(mContext).inflate(R.layout.pager_item, container, false);
         ImageView mImg = (ImageView) view.findViewById(R.id.pager_img);
         TextView mTitle = (TextView) view.findViewById(R.id.pager_title);
+        TextView moreVideo = (TextView) view.findViewById(R.id.morevideo);
+
+        moreVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.startActivity(new Intent(mContext,VideoListActivity.class));
+            }
+        });
 
         Glide.with(mContext).load(tops.get(position).getVF_IMG_URL()).into(mImg);
         mTitle.setText(tops.get(position).getVF_VIDEO_NAME());
-//        final int id = tops.get(position).getId();
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                DailyDetailActivity.lanuch(mContext, id);
                 /**
                  * 点击事件
                  */
+                Toast.makeText(mContext,"点击了第"+position+"项",Toast.LENGTH_SHORT).show();
+                mIntent = new Intent(mContext, VideoActivity.class);
+                mIntent.putExtra("url", tops.get(position).getVF_VIDEO_PATH());
+                mContext.startActivity(mIntent);
 
             }
         });
